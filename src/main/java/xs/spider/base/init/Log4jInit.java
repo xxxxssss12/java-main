be7161114838206ea0024a7c685bfdad9614e5e0
xiongshun <xiongshun@wecash.com>
2017-04-01 18:58:33 +0800

    spider

--- src/main/java/xs/spider/base/init/Log4jInit.java
+++ src/main/java/xs/spider/base/init/Log4jInit.java
@@ -1,8 +1,13 @@
 package xs.spider.base.init;
 
+import org.apache.log4j.PropertyConfigurator;
+import xs.spider.base.config.ConfigProvider;
+
 /**
  * Created by xs on 2017/4/1.
  */
 public class Log4jInit {
-
+    public static void init() {
+        PropertyConfigurator.configure(ConfigProvider.getProp());//装入log4j配置信息
+    }
 }
