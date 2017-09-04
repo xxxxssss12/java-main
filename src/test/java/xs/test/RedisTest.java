c075220fc59183d56836fca35b4766a663cc7046
xiongshun <xiongshun@wecash.net>
2017-09-04 16:32:36 +0800

    wan shan

--- src/test/java/xs/test/RedisTest.java
+++ src/test/java/xs/test/RedisTest.java
@@ -6,7 +6,26 @@ import xs.spider.base.util.RedisConnector;
  * Created by xs on 2017/8/31
  */
 public class RedisTest {
+
+    public static void createTime() {
+        Long timestamp = System.currentTimeMillis();
+        long sequence = RedisConnector.incr("test");
+        String seq = String.format("%03d", sequence % 1000);
+        if (RedisConnector.setnx("test:" + seq + timestamp) <= 0) {
+            System.out.println("test:" + seq + timestamp + "已存在！！！incr=" +sequence);
+        }
+        if (sequence % 10000 == 0) System.out.println(sequence);
+    }
     public static void main(String[] args) {
-        System.out.println(RedisConnector.get("hehe"));
+        for (int i=0; i<30; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int j=0; j<100000; j++) {
+                        createTime();
+                    }
+                }
+            }).start();
+        }
     }
 }
