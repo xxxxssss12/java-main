4dd0ae41a44e7738976f6cd925f41b90650ae4fd
xiongshun <xiongshun@wecash.net>
2018-10-21 12:09:04 +0800

    'ok'

--- src/main/java/xs/spider/base/util/Util.java
+++ src/main/java/xs/spider/base/util/Util.java
@@ -2,6 +2,9 @@ package xs.spider.base.util;
 
 import org.apache.log4j.Logger;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,6 +13,7 @@ import java.math.BigDecimal;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
@@ -124,42 +128,84 @@ public class Util {
 		else return obj.toString();
 	}
 	/**
+	 * 获取用户真实IP地址，不使用request.getRemoteAddr()的原因是有可能用户使用了代理软件方式避免真实IP地址,
+	 * 可是，如果通过了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP值
+	 *
+	 * @return ip
+	 */
+	public static String getIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		System.out.println("x-forwarded-for ip: " + ip);
+		if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+			// 多次反向代理后会有多个ip值，第一个ip才是真实ip
+			if( ip.indexOf(",")!=-1 ){
+				ip = ip.split(",")[0];
+			}
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+			System.out.println("Proxy-Client-IP ip: " + ip);
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+			System.out.println("WL-Proxy-Client-IP ip: " + ip);
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_CLIENT_IP");
+			System.out.println("HTTP_CLIENT_IP ip: " + ip);
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+			System.out.println("HTTP_X_FORWARDED_FOR ip: " + ip);
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("X-Real-IP");
+			System.out.println("X-Real-IP ip: " + ip);
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+			System.out.println("getRemoteAddr ip: " + ip);
+		}
+		System.out.println("获取客户端ip: " + ip);
+		return ip;
+	}
+	/**
 	 * 获取请求中的信息
 	 * @return*/
-//	public static Map<String, Object> getrequestInfo(ServletRequest request) {
-//		HttpServletRequest httpreq = (HttpServletRequest) request;
-//		//ȡcookies
-//		Cookie[] cookies = httpreq.getCookies();
-//		Map<String, String> cookiemap = new HashMap<String, String>();
-//		if (!isBlank(cookies)) {
-//			for (Cookie cookie : cookies) {
-//				String name = cookie.getName();
-//				String value = cookie.getValue() + ";path=" + cookie.getPath();
-//				cookiemap.put(name, value);
-//			}
-//		}
-//		Enumeration<String> headernames = httpreq.getHeaderNames();
-//		//ȡheader
-//		Map<String, String> headermap = new HashMap<String, String>();
-//		while (headernames.hasMoreElements()) {
-//			String name = headernames.nextElement();
-//			String value = httpreq.getHeader(name);
-//			headermap.put(name, value);
-//		}
-//		//ȡparam
-//		Map<String, String[]> tempparammap = httpreq.getParameterMap();
-//		Map<String, String> parammap = new HashMap<String , String>();
-//		if (!isBlank(tempparammap)) {
-//			for (Entry<String,String[]> entry : tempparammap.entrySet()) {
-//				parammap.put(entry.getKey(), entry.getValue()[0]);
-//			}
-//		}
-//		Map<String, Object> all = new HashMap<String, Object>();
-//		all.put("cookies", cookiemap);
-//		all.put("headers", headermap);
-//		all.put("params", parammap);
-//		return all;
-//	}
+	public static Map<String, Object> getrequestInfo(ServletRequest request) {
+		HttpServletRequest httpreq = (HttpServletRequest) request;
+		//ȡcookies
+		Cookie[] cookies = httpreq.getCookies();
+		Map<String, String> cookiemap = new HashMap<String, String>();
+		if (!isBlank(cookies)) {
+			for (Cookie cookie : cookies) {
+				String name = cookie.getName();
+				String value = cookie.getValue() + ";path=" + cookie.getPath();
+				cookiemap.put(name, value);
+			}
+		}
+		Enumeration<String> headernames = httpreq.getHeaderNames();
+		//ȡheader
+		Map<String, String> headermap = new HashMap<String, String>();
+		while (headernames.hasMoreElements()) {
+			String name = headernames.nextElement();
+			String value = httpreq.getHeader(name);
+			headermap.put(name, value);
+		}
+		//ȡparam
+		Map<String, String[]> tempparammap = httpreq.getParameterMap();
+		Map<String, String> parammap = new HashMap<String , String>();
+		if (!isBlank(tempparammap)) {
+			for (Entry<String,String[]> entry : tempparammap.entrySet()) {
+				parammap.put(entry.getKey(), entry.getValue()[0]);
+			}
+		}
+		Map<String, Object> all = new HashMap<String, Object>();
+		all.put("cookies", cookiemap);
+		all.put("headers", headermap);
+		all.put("params", parammap);
+		return all;
+	}
 	public static String toLowFirst(String str) {
 		try {
 			if (isBlank(str)) return str;
